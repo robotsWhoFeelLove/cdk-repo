954f7c1e6f229fdb3c7d61c3e0375158ac735119
QuantumWorkMechanics <132368002+robotsWhoSmokeCrack@users.noreply.github.com>
2024-01-18 13:57:27 -0500

    Update pipeline with test stage

--- lib/cicd-cdk-stack.ts
+++ lib/cicd-cdk-stack.ts
@@ -1,18 +1,25 @@
 import * as cdk from "aws-cdk-lib";
 import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
+import { PipelineStage } from "./PipelineStage";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CicdCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, "AwesomePipeline", {
+    const pipeline = new CodePipeline(this, "AwesomePipeline", {
       pipelineName: "AwsomePipeline",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub("robotsWhoFeelLove/cdk-repo", "main"),
         commands: ["npm ci", "npx cdk synth"],
       }),
     });
+
+    const testStage = pipeline.addStage(
+      new PipelineStage(this, "PipelineTestStage", {
+        stageName: "test",
+      })
+    );
   }
 }
